075d9615c19b82c4f68cdda06d05a685582f29cb
Drew Fleeman <drew.fleeman@gmail.com>
2017-09-20 17:59:43 -0500

    add tab bar and status bar

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Platform, StatusBar } from 'react-native'
 import DeckList from './components/DeckList'
 import DeckDetail from './components/DeckDetail'
-import { StackNavigator } from 'react-navigation'
+import NewDeck from './components/NewDeck'
+import { StackNavigator, TabNavigator } from 'react-navigation'
+import { Entypo } from '@expo/vector-icons'
+import { black, white } from './utils/colors'
+import { Constants } from 'expo'
 
 const Stack = StackNavigator({
   DeckList: {
@@ -13,10 +17,42 @@ const Stack = StackNavigator({
   }
 })
 
+const Tabs = TabNavigator({
+  Decks: {
+    screen: Stack,
+    navigationOptions: {
+      tabBarLabel: 'My Decks',
+      tabBarIcon: ({ tintColor }) => <Entypo name='list' size={30} color={tintColor} />
+    }
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'Create Deck',
+      tabBarIcon: ({ tintColor }) => <Entypo name='add-to-list' size={30} color={tintColor} />
+    }
+  }
+}, {
+  tabBarOptions: {
+    activeTintColor: black
+  }
+})
+
+function FlashcardsStatusBar({...props}) {
+  return (
+    <View style={{ height: Constants.statusBarHeight }}>
+      <StatusBar translucent {...props} />
+    </View>
+  )
+}
+
 export default class App extends React.Component {
   render() {
     return (
-      <Stack />
+      <View style={{flex: 1}}>
+        <FlashcardsStatusBar />
+        <Tabs/>
+      </View>
     );
   }
 }
